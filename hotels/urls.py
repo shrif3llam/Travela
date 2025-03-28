361646bdae4b98e3140e3a62faa1c6a5e237ff3a
shrif3llam <109836383+shrif3llam@users.noreply.github.com>
2025-03-28 22:01:09 +0200

    finish Headers and tow Govers

--- hotels/urls.py
+++ hotels/urls.py
@@ -2,7 +2,18 @@ from django.urls import path , include
 from . import views
 from .views import hotel_detail
 urlpatterns = [
-    path('',views.index,name='index'),
+    #Header
+    path('',views.index,name='home'),
+    path('about',views.index,name='about'),
+    path('booking',views.Booking,name='booking'),
+    path('search',views.index,name='search'),
+    path('travel',views.Travel,name='travel'),
+    path('contact',views.Contact,name='contact'),
+    path('pages',views.index,name='pages'),
     path('hotels',views.hotel_list),
     path('hotels/<int:id>/',views.hotel_detail),
+    
+    #Govers
+    path('govers/alex',views.Alex,name='alex'),
+    path('govers/cairo',views.Cairo,name='cairo'),
 ]
--- hotels/views.py
+++ hotels/views.py
@@ -1,17 +1,45 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Hotel
 # Create your views here.
-def hotel_list(request):
-    hotel_list=Hotel.objects.all()
-    context={'hotels':hotel_list}
-    return render(request,'hotels/hotel_list.html',context)
+
+#Header
 
 def index(request):
     rooms = Hotel.objects.all()
     context = {'rooms': rooms}
     return render(request,'hotels/index.html',context)
 
+def Contact(request):
+    return render(request,'hotels/contact.html')
+
+def Travel(request):
+    return render(request,'hotels/Travel.html')
+
+def Booking(request):
+    rooms = Hotel.objects.all()
+    context = {'rooms': rooms}
+    return render(request,'hotels/booking.html',context)
+
+def hotel_list(request):
+    hotel_list=Hotel.objects.all()
+    context={'hotels':hotel_list}
+    return render(request,'hotels/hotel_list.html',context)
 def hotel_detail(request, id):
     hotel_detail=Hotel.objects.get(id=id)
     context={'hotel':hotel_detail}
     return render(request,'hotels/hotel_detail.html',context)
+
+
+
+
+#Govers
+
+def Alex(request):
+    rooms = Hotel.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'hotels/Govers/alex.html', context)
+
+def Cairo(request):
+    rooms = Hotel.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'hotels/Govers/cairo.html', context)
